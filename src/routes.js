24b26eee1bbbf7cd8d0a2735afcb129c3807035f
Joel Przybylowski <przybylowski@Macintosh-2.local>
2016-07-03 11:31:28 -0500

    Made SubjectsPage plural

--- src/routes.js
+++ src/routes.js
@@ -3,14 +3,14 @@ import {Route, IndexRoute} from 'react-router';
 import App from './components/App';
 import HomePage from './components/home/HomePage';
 import SearchPage from './components/search/SearchPage';
-import SubjectPage from './components/subject/SubjectPage';
+import SubjectsPage from './components/subject/SubjectsPage';
 import TutorPage from './components/tutor/TutorPage';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={HomePage} />
     <Route path="search" component={SearchPage} />
-    <Route path="subjects" component={SubjectPage} />
+    <Route path="subjects" component={SubjectsPage} />
     <Route path="tutors" component={TutorPage} />
   </Route>
 );
